f6d491ba2bc7dcbda8e514cc2f36ebe9481d5c97
wselemani <selemaniwezzie5@gmail.com>
2026-06-24 16:29:51 -0700

    my last commit

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__ == '__main__':
-    names = dir(hidden_4)
-    names.sort()
-    for attr in names:
-        if attr[:2] != "__":
-            print(attr)
+if __name__ == "__main__":
+    import variable_load_5
+    print(variable_load_5.a)
