bceddef853a806c594769d4f449ef5cf26912cc0
wselemani <selemaniwezzie5@gmail.com>
2026-07-19 06:51:19 -0700

    adding 9files and readme

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+"""Checks object class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks object class
+    Args:
+        - obj: object to class
+        - a_class: class to check
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+please ream me
