5a3af4c1c8612fa8073b1170234fe187985bfbf7
wselemani <selemaniwezzie5@gmail.com>
2026-07-20 11:05:21 -0700

    add City model and fetch script

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,15 +1,13 @@
-"""
-Contains class City
-"""
-
-import sqlalchemy
+#!/usr/bin/python3
+"""Module for City class"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from model_state import Base
 
 
 class City(Base):
-    """Representation of a city"""
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    """City class that links to cities table"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
