37e6828e62e5bbf62d15247c7edabdaeda87de39
wselemani <selemaniwezzie5@gmail.com>
2026-07-21 14:52:53 -0700

    Use correct URL

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
