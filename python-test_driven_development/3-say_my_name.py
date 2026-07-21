7ca496818cbc2f7c5e2add569ce6189f96835ba9
wselemani <selemaniwezzie5@gmail.com>
2026-07-21 15:36:54 -0700

    Fix say_my_name formatting and push tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""Say my name function."""
+"""This module supplies one function, say_my_name(first_name, last_name="")."""
 
 
 def say_my_name(first_name, last_name=""):
-    """prints my name."""
+    """Prints 'My name is <first name> <last name>'.
+
+    Args:
+        first_name: The first name string.
+        last_name: The last name string, defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
+
+    print("My name is {} {}".format(first_name, last_name))
